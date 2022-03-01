f316a808d066cdacbe1f8ede9f131ebc133be1a5
dominikvaradi <varadidominik2000@gmail.com>
2022-03-01 11:15:41 +0100

    Initial Commit
    Back-end directory created with initial Spring Boot IDEA Project

---
+++ backend/src/main/java/hu/dominikvaradi/rentialwebapp/RentialWebappApplication.java
@@ -0,0 +1,13 @@
+package hu.dominikvaradi.rentialwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentialWebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentialWebappApplication.class, args);
+    }
+
+}
